638b0b884b996bf17d35f0f4983b04506e9c67c9
Amanda Keogh <amanda.keogh@gmail.com>
2015-11-23 11:50:56 -0800

    up to page 47

---
+++ index.js
@@ -0,0 +1,10 @@
+var server = require(__dirname + "/server.js");
+var router = require(__dirname + "/router.js");
+var requestHandlers = require(__dirname + "/requestHandlers.js");
+
+var handle = {};
+handle["/"] = requestHandlers.start;
+handle["/start"] = requestHandlers.start;
+handle["/upload"] = requestHandlers.upload;
+
+server.start(router.route, handle);
